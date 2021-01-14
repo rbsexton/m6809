b2512632185f6ff78fb6706319989ef08abefe48
rbsexton <robertbsexton@gmail.com>
2021-01-14 06:44:55 -0800

    Create a RAM and hook it up.    Untested.

--- m6809_integration.v
+++ m6809_integration.v
@@ -26,12 +26,31 @@ wire        data_rw_n;
 // Ram/Rom here
 // --------------------------------------------------------------------
 
-// Address decode later.
+// Divide the memory space in half
+wire sel_rom =  address[15];
+wire sel_ram = ~address[15];
+
+wire [ 7:0] core_data_in_rom;
+wire [ 7:0] core_data_in_ram;
+
+// Mux data from memory devices.
+assign core_data_in = {
+  sel_rom ? core_data_in_rom :
+  core_data_in_ram
+  };
 
 rom_boot u_rom (
-  .sel                   (1'b1),
+  .sel                   (sel_rom),
   .a                     (address[7:0]),
-  .dout                  (core_data_in)
+  .dout                  (core_data_in_rom)
+  );
+
+ram u_ram (
+  .clk                   (clk), 
+  .sel                   (sel_ram),
+  .a                     (address[7:0]),
+  .dout                  (core_data_in_ram),
+  .din                   (core_data_out)
   );
 
 // --------------------------------------------------------------------
---
+++ ram.v
@@ -0,0 +1,23 @@
+// Emulate an external sync memory.
+
+module ram (
+
+  input  wire clk,
+  input  wire sel,
+  input  wire wr_n,
+  
+  input  wire  [7:0] a,
+
+  output wire  [7:0] dout,
+  input  wire  [7:0] din
+  );
+
+  reg [7:0] mem [255:0];
+
+always @(posedge clk) begin
+  if ( ~wr_n & sel ) mem[a] <= din;
+  end
+  
+assign dout = mem[a];   
+
+endmodule
